7d598ac12b7ce7a44eae314814467e219397e9bf
Ilteber Ayvaci <ilteber.ayvaci@solity.org>
2023-11-19 07:25:54 +0300

    Config cannot be reached, for a reason we dont know :(

--- packages/chain/src/LendingMarkets/LendingMarket.ts
+++ packages/chain/src/LendingMarkets/LendingMarket.ts
@@ -1,11 +1,11 @@
 import {
-    RuntimeModule,
-    runtimeModule,
-    state,
-    runtimeMethod,
-  } from "@proto-kit/module";
-  import { State, StateMap, assert } from "@proto-kit/protocol";
-  import { Bool, PublicKey, UInt64 , Field} from "o1js";
+  RuntimeModule,
+  runtimeModule,
+  state,
+  runtimeMethod,
+} from "@proto-kit/module";
+import { State, StateMap, assert } from "@proto-kit/protocol";
+import { Bool, PublicKey, UInt64, Field } from "o1js";
 import { ERC20LikeToken } from "../tokens/ERC20LikeToken";
 import { inject } from "tsyringe";
 import { zkMToken } from "../tokens/zkMToken";
@@ -17,125 +17,144 @@ export const errors = {
   tokenInAmountTooHigh: () => "Token amount too high",
 };
 interface LendingMarketConfig {
-  minaTokenAddresses : Map<string, string> ;
-  minaMTokenAddresses : Map<string, string> ;
-  minaMDebtTokenAddresses : Map<string, string> ;
-  LendingMarketAddr : string ;
+  minaMTokenAddresses: Map<string, string>;
+  minaTokenAddresses: Map<string, string>;
+  minaMDebtTokenAddresses: Map<string, string>;
+  LendingMarketAddr: string;
 }
 @runtimeModule()
 export class LendingMarket extends RuntimeModule<LendingMarketConfig> {
-
   @state() public mSupplyToken = StateMap.from<PublicKey, UInt64>(
     PublicKey,
     UInt64
   );
-  public constructor(@inject("zkMToken") public  zkmTokenMap: Map<PublicKey, zkMToken>,@inject("zkMDebtToken") public  zkmDebtTokenMap: Map<PublicKey, zkMDebtToken>,@inject("ERC20LikeToken") public  tokenMap: Map<PublicKey, ERC20LikeToken>) {
+  public constructor(
+    @inject("zkMToken") public zkmTokenMap: Map<PublicKey, zkMToken>,
+    @inject("zkMDebtToken")
+    public zkmDebtTokenMap: Map<PublicKey, zkMDebtToken>,
+    @inject("ERC20LikeToken") public tokenMap: Map<PublicKey, ERC20LikeToken>
+  ) {
     super();
-    this.config.minaTokenAddresses.forEach((value, key) => {
-      const mtoken = new zkMToken()
-      const mtokenAddr = this.config.minaMTokenAddresses.get(key)
+  }
+  public async init(): Promise<void> {
+    await this.config.minaTokenAddresses.forEach((value, key) => {
+      const mtoken = new zkMToken();
+      const mtokenAddr = this.config.minaMTokenAddresses.get(key);
       if (mtokenAddr == undefined) {
-        mtoken.setTokenAddress(PublicKey.from({x:Field.from(""),isOdd:Bool(true)})); 
-      } else{
-        mtoken.setTokenAddress(PublicKey.from({x:Field.from(mtokenAddr),isOdd:Bool(true)})); 
-
+        mtoken.setTokenAddress(
+          PublicKey.from({ x: Field.from(""), isOdd: Bool(true) })
+        );
+      } else {
+        mtoken.setTokenAddress(
+          PublicKey.from({ x: Field.from(mtokenAddr), isOdd: Bool(true) })
+        );
       }
-      mtoken.setAdmin(PublicKey.from({x:Field.from(this.config.LendingMarketAddr),isOdd:Bool(true)}))
-      mtoken.setSymbol(Field.from("zk"+key+"MToken"));
+      mtoken.setAdmin(
+        PublicKey.from({
+          x: Field.from(this.config.LendingMarketAddr),
+          isOdd: Bool(true),
+        })
+      );
+      mtoken.setSymbol(Field.from("zk" + key + "MToken"));
 
-      zkmTokenMap.set(PublicKey.from({x:Field.from(key),isOdd:Bool(true)}),mtoken);
+      this.zkmTokenMap.set(
+        PublicKey.from({ x: Field.from(key), isOdd: Bool(true) }),
+        mtoken
+      );
 
-      const mDebttoken = new zkMDebtToken()
-      const mDebttokenAddr = this.config.minaMDebtTokenAddresses.get(key)
+      const mDebttoken = new zkMDebtToken();
+      const mDebttokenAddr = this.config.minaMDebtTokenAddresses.get(key);
       if (mDebttokenAddr == undefined) {
-        mtoken.setTokenAddress(PublicKey.from({x:Field.from(""),isOdd:Bool(true)})); 
-      } else{
-        mtoken.setTokenAddress(PublicKey.from({x:Field.from(mDebttokenAddr),isOdd:Bool(true)})); 
-
+        mtoken.setTokenAddress(
+          PublicKey.from({ x: Field.from(""), isOdd: Bool(true) })
+        );
+      } else {
+        mtoken.setTokenAddress(
+          PublicKey.from({ x: Field.from(mDebttokenAddr), isOdd: Bool(true) })
+        );
       }
-      mDebttoken.setAdmin(PublicKey.from({x:Field.from(this.config.LendingMarketAddr),isOdd:Bool(true)}))
-      mDebttoken.setSymbol(Field.from("zk"+key+"DebtToken"));
+      mDebttoken.setAdmin(
+        PublicKey.from({
+          x: Field.from(this.config.LendingMarketAddr),
+          isOdd: Bool(true),
+        })
+      );
+      mDebttoken.setSymbol(Field.from("zk" + key + "DebtToken"));
 
-      zkmDebtTokenMap.set(PublicKey.from({x:Field.from(key),isOdd:Bool(true)}),mtoken);
+      this.zkmDebtTokenMap.set(
+        PublicKey.from({ x: Field.from(key), isOdd: Bool(true) }),
+        mtoken
+      );
 
-      const erc20token = new ERC20LikeToken()
-      const erc20TokenAddr = value
+      const erc20token = new ERC20LikeToken();
+      const erc20TokenAddr = value;
       if (erc20TokenAddr == undefined) {
-        mtoken.setTokenAddress(PublicKey.from({x:Field.from(""),isOdd:Bool(true)})); 
-      } else{
-        mtoken.setTokenAddress(PublicKey.from({x:Field.from(erc20TokenAddr),isOdd:Bool(true)})); 
-
+        mtoken.setTokenAddress(
+          PublicKey.from({ x: Field.from(""), isOdd: Bool(true) })
+        );
+      } else {
+        mtoken.setTokenAddress(
+          PublicKey.from({ x: Field.from(erc20TokenAddr), isOdd: Bool(true) })
+        );
       }
       erc20token.setSymbol(Field.from(key));
 
-      tokenMap.set(PublicKey.from({x:Field.from(key),isOdd:Bool(true)}),new ERC20LikeToken());
+      this.tokenMap.set(
+        PublicKey.from({ x: Field.from(key), isOdd: Bool(true) }),
+        new ERC20LikeToken()
+      );
     });
-
-
-  }
-  public assertTokenExist(asset:PublicKey) {
-    
+    }
+  public assertTokenExist(asset: PublicKey) {
     assert(this.tokenExist(asset), errors.tokenDoesNotExist());
   }
-  public assertZKMTokenExist(asset:PublicKey) {
-    
+  public assertZKMTokenExist(asset: PublicKey) {
     assert(this.ZKMTokenExist(asset), errors.tokenDoesNotExist());
   }
-  public assertBalanceSufficient( userBalance:UInt64, amount:UInt64) {
-    const isTokenOutAmountSufficient =
-    userBalance.greaterThanOrEqual(amount);
+  public assertBalanceSufficient(userBalance: UInt64, amount: UInt64) {
+    const isTokenOutAmountSufficient = userBalance.greaterThanOrEqual(amount);
     assert(isTokenOutAmountSufficient, errors.tokenOutAmountTooLow());
-
   }
   public tokenExist(asset: PublicKey) {
     const pool = this.tokenMap.get(asset);
 
-    if(pool === undefined)
-      return Bool(false);
-    else
-      return Bool(true);
+    if (pool === undefined) return Bool(false);
+    else return Bool(true);
   }
   public ZKMTokenExist(asset: PublicKey) {
     const pool = this.zkmTokenMap.get(asset);
 
-    if(pool === undefined)
-      return Bool(false);
-    else
-      return Bool(true);
+    if (pool === undefined) return Bool(false);
+    else return Bool(true);
   }
   @runtimeMethod()
   public supply(asset: PublicKey, user: PublicKey, amount: UInt64): void {
     this.assertTokenExist(asset);
     const token = this.tokenMap.get(asset);
-    if(token === undefined)
-      return;
-    const balance = token.balanceOf(user)
+    if (token === undefined) return;
+    const balance = token.balanceOf(user);
     this.assertBalanceSufficient(balance, amount);
     this.assertZKMTokenExist(asset);
 
-    const zkmToken = this.zkmTokenMap.get(asset)
-    if(zkmToken === undefined)
-      return;
+    const zkmToken = this.zkmTokenMap.get(asset);
+    if (zkmToken === undefined) return;
     const mTokenAddress = zkmToken.getAddress();
-    token.transfer(user,mTokenAddress, amount);
+    token.transfer(user, mTokenAddress, amount);
     zkmToken._supply(user, amount);
-
   }
 
   @runtimeMethod()
   public withdraw(asset: PublicKey, user: PublicKey, amount: UInt64): void {
     this.assertZKMTokenExist(asset);
-    const zkmToken = this.zkmTokenMap.get(asset)
-    if(zkmToken === undefined)
-      return;
+    const zkmToken = this.zkmTokenMap.get(asset);
+    if (zkmToken === undefined) return;
     zkmToken._withdraw(user, amount);
     const mTokenAddress = zkmToken.getAddress();
     const token = this.tokenMap.get(asset);
-    if(token === undefined)
-      return;
-    token.transfer(mTokenAddress,user, amount);
+    if (token === undefined) return;
+    token.transfer(mTokenAddress, user, amount);
   }
-  
+
   @runtimeMethod()
   public borrow(asset: PublicKey, user: PublicKey, amount: UInt64): void {
     // this.assertZKMTokenExist(asset);
@@ -147,7 +166,7 @@ export class LendingMarket extends RuntimeModule<LendingMarketConfig> {
     //   return;
     // zkmDebtToken._borrow(user, amount);
   }
-  
+
   @runtimeMethod()
   public repay(asset: PublicKey, user: PublicKey, amount: UInt64): void {}
 }
--- packages/chain/src/runtime.ts
+++ packages/chain/src/runtime.ts
@@ -1,8 +1,8 @@
 import { UInt64 } from "o1js";
-import {  LendingMarket} from "./LendingMarkets/LendingMarket";
-import {  zkMToken} from "./tokens/zkMToken";
-import {  zkMDebtToken} from "./tokens/zkMDebtToken";
-import {  ERC20LikeToken} from "./tokens/ERC20LikeToken";
+import { LendingMarket } from "./LendingMarkets/LendingMarket";
+import { zkMToken } from "./tokens/zkMToken";
+import { zkMDebtToken } from "./tokens/zkMDebtToken";
+import { ERC20LikeToken } from "./tokens/ERC20LikeToken";
 
 export default {
   modules: {
@@ -13,26 +13,25 @@ export default {
   },
   config: {
     LendingMarket: {
-    minaTokenAddresses : new Map<string, string>([
-    ["USDT", "B62qpZ8L4QZVb9vPX7X4ZJdQoM8iW4e9FqD4xjR8PakjDcTNmJm3Ws6"],
-    ["ETH", "B62qjLkpE4ctW5A6rPU4N5P3C5xW6CwKjK8QmxEFHtrUx2QKvT2VbGP"],
-    ["DAI", "B62qmXWTU7GsM3M8ZexGe7QYmbGz1CvVUvvmojX5rEJmCypqkqZARsT"],
-    ["AAVE", "B62qrF4Q62vM8F3MdjbCq4C9Vz1kV3M4fH2RsvMh8Xy2mB4S8Yq4SbG"],
-    ]),
-    minaMTokenAddresses : new Map<string, string>([
-      ["ETH", "B62qkK9MiVzFgY8RRSSWxj6YBqW6DpMfU95CQUA29jnEndkwF5RjV6N"],
-      ["DAI", "B62qiZJYPcTjWtqG9k2qF4DxPbHjR4QkC8kK1gwBmpQsHxsVYD3a8Ea"],
-      ["AAVE", "B62qs9G6G2Gc8Pf2J2GxH6FkJSbND8YbQ2s5UW8uP3vWAb4s8YVajEz"],
-      ["USDT", "B62qrJ8yD3uz5CqVW8nXsM4B3M4G8M7GkGxEk3jG2xjZPc8xH3Zs2Hn"],
-  ]),
-    minaMDebtTokenAddresses : new Map<string, string>([
-      ["ETH", "B62qnR5JU6xVvJZ6eVfRxU4GTBwX4yT6x4uFZBxV8wvEMz6V3kYVbTi"],
-      ["DAI", "B62qj7CGRyV4nH5JTFLc3mD8BfF6MmMkDfGqB3H6Pv4oQbB5mVsjR3V"],
-      ["USDT", "B62qmE9JMGV9xUvcTfR9Q8tq5SyB5PcjH9jPw7CQU7zB5pHfK7DmCZx"],
-      ["AAVE", "B62qnFhPCVV5oPqfyrzT5zmJ7EY4PqAa4UXnHh8DhAVkHrR6s3K5ZB3"],
-      
-  ]),
-    LendingMarketAddr :  "B62qrJ8yD3uz5CqVW8nXsM4B3M4G8M7GjGxEk3jG2xjZPc8xH3Zs2Hn",
+      minaTokenAddresses: new Map<string, string>([
+        ["USDT", "B62qpZ8L4QZVb9vPX7X4ZJdQoM8iW4e9FqD4xjR8PakjDcTNmJm3Ws6"],
+        ["ETH", "B62qjLkpE4ctW5A6rPU4N5P3C5xW6CwKjK8QmxEFHtrUx2QKvT2VbGP"],
+        ["DAI", "B62qmXWTU7GsM3M8ZexGe7QYmbGz1CvVUvvmojX5rEJmCypqkqZARsT"],
+        ["AAVE", "B62qrF4Q62vM8F3MdjbCq4C9Vz1kV3M4fH2RsvMh8Xy2mB4S8Yq4SbG"],
+      ]),
+      minaMTokenAddresses: new Map<string, string>([
+        ["ETH", "B62qkK9MiVzFgY8RRSSWxj6YBqW6DpMfU95CQUA29jnEndkwF5RjV6N"],
+        ["DAI", "B62qiZJYPcTjWtqG9k2qF4DxPbHjR4QkC8kK1gwBmpQsHxsVYD3a8Ea"],
+        ["AAVE", "B62qs9G6G2Gc8Pf2J2GxH6FkJSbND8YbQ2s5UW8uP3vWAb4s8YVajEz"],
+        ["USDT", "B62qrJ8yD3uz5CqVW8nXsM4B3M4G8M7GkGxEk3jG2xjZPc8xH3Zs2Hn"],
+      ]),
+      minaMDebtTokenAddresses: new Map<string, string>([
+        ["ETH", "B62qnR5JU6xVvJZ6eVfRxU4GTBwX4yT6x4uFZBxV8wvEMz6V3kYVbTi"],
+        ["DAI", "B62qj7CGRyV4nH5JTFLc3mD8BfF6MmMkDfGqB3H6Pv4oQbB5mVsjR3V"],
+        ["USDT", "B62qmE9JMGV9xUvcTfR9Q8tq5SyB5PcjH9jPw7CQU7zB5pHfK7DmCZx"],
+        ["AAVE", "B62qnFhPCVV5oPqfyrzT5zmJ7EY4PqAa4UXnHh8DhAVkHrR6s3K5ZB3"],
+      ]),
+      LendingMarketAddr: "B62qrJ8yD3uz5CqVW8nXsM4B3M4G8M7GjGxEk3jG2xjZPc8xH3Zs2Hn"
     },
     zkMDebtToken: {},
     zkMToken: {},
